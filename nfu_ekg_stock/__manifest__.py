a7308a8792d9e1219f11a66a84f7fc3f32a26b78
Niels Göttsch <ng@ife.de>
2024-05-02 17:00:44 +0200

    [imp] first version of EKG delivery slip

---
+++ nfu_ekg_stock/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "NFU EKG Stock",
+    "summary": "NFU EKG Stock Improvements",
+    "author": "BAKEUP",
+    "website": "https://www.bakeup.org",
+    "category": "Stock",
+    "version": "16.0.1.0.1",
+    "depends": ["stock"],
+    "data": ["views/report_deliveryslip.xml", "report/stock_report_views.xml"],
+    "assets": {"web.report_assets_common": ["nfu_ekg_stock/static/src/scss/report_stock_rule.scss"]},
+    "license": "LGPL-3",
+    "application": True,
+}
