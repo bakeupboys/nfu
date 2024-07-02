bc3862bd500da43abc6773abb8cd1796c2ec4923
Matthias Brück <hi@brueck.io>
2024-07-02 10:29:45 +1000

    Add constraint

--- nfu_sale_product_min_max_qty/models/sale_order_line.py
+++ nfu_sale_product_min_max_qty/models/sale_order_line.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class SaleOrderLine(models.Model):
@@ -6,3 +7,9 @@ class SaleOrderLine(models.Model):
 
     product_uom_min_qty = fields.Float(string="Min qty.", digits="Product Unit of Measure")
     product_uom_max_qty = fields.Float(string="Max qty.", digits="Product Unit of Measure")
+
+    @api.constrains("product_uom_qty")
+    def _check_product_uom_qty(self):
+        for record in self:
+            if record.product_uom_qty > record.product_uom_max_qty:
+                raise ValidationError("The quantity must be less than or equal to the maximum quantity.")
