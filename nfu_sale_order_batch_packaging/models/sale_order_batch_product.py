1a77c5ed98cdf6723cc68bba9be0c6a8b42811fc
Niels Göttsch <ng@ife.de>
2025-03-28 16:27:14 +0100

    [IMP] nfu_sale_order_batch_packaging:  add open_max_qty

--- nfu_sale_order_batch_packaging/models/sale_order_batch_product.py
+++ nfu_sale_order_batch_packaging/models/sale_order_batch_product.py
@@ -8,33 +8,45 @@ class SaleOrderBatchProduct(models.Model):
     _inherit = "sale.order.batch.product"
 
     product_uom_max_qty = fields.Float("Max Qty", compute="_compute_product_uom_max_qty")
-    open_packaging_qty = fields.Float(compute="_compute_open_packagin_qty", store=True)
-    open_packaging_state = fields.Selection(selection=PACKAGING_STATES, compute="_compute_open_packagin_state")
+    open_packaging_qty = fields.Float(compute="_compute_open_packaging_qty")
+    open_packaging_max_qty = fields.Float(compute="_compute_open_packaging_max_qty")
+    open_packaging_state = fields.Selection(selection=PACKAGING_STATES, compute="_compute_open_packaging_state")
 
     @api.depends("sale_order_line_ids.product_uom_max_qty")
     def _compute_product_uom_max_qty(self):
         for product in self:
-            if product.sale_order_line_ids:
-                product.product_uom_max_qty = sum(product.sale_order_line_ids.mapped("product_uom_max_qty"))
-            else:
-                product.product_uom_max_qty = False
+            product.product_uom_max_qty = sum(product.sale_order_line_ids.mapped("product_uom_max_qty"))
 
     @api.depends("product_uom_qty")
-    def _compute_open_packagin_qty(self):
+    def _compute_open_packaging_qty(self):
         for product in self:
-            open_packaging_qty = product.product_packaging_qty - (
-                product.product_uom_qty % product.product_packaging_qty
-            )
-            product.open_packaging_qty = (
-                0 if open_packaging_qty == product.product_packaging_qty else open_packaging_qty
-            )
+            if product.product_packaging_id:
+                open_packaging_qty = product.product_packaging_qty - (
+                    product.product_uom_qty % product.product_packaging_qty
+                )
+                product.open_packaging_qty = (
+                    0 if open_packaging_qty == product.product_packaging_qty else open_packaging_qty
+                )
+            else:
+                product.open_packaging_qty = 0.0
+
+    @api.depends("product_uom_max_qty")
+    def _compute_open_packaging_max_qty(self):
+        for product in self:
+            if product.product_packaging_id:
+                if product.product_uom_max_qty < product.product_uom_qty + product.open_packaging_qty:
+                    product.open_packaging_max_qty = product.product_packaging_qty - (
+                        product.product_uom_max_qty % product.product_packaging_qty
+                    )
+                else:
+                    product.open_packaging_max_qty = 0.0
+            else:
+                product.open_packaging_max_qty = 0.0
 
     @api.depends("open_packaging_qty")
-    def _compute_open_packagin_state(self):
+    def _compute_open_packaging_state(self):
         for product in self:
-            if product.open_packaging_qty == 0:
-                product.open_packaging_state = "full"
-            elif (product.product_uom_max_qty - product.product_uom_qty) >= product.open_packaging_qty:
+            if product.open_packaging_qty == 0 or product.open_packaging_max_qty == 0:
                 product.open_packaging_state = "full"
             elif product.product_uom_qty < product.product_packaging_qty:
                 product.open_packaging_state = "open"
