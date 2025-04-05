97b505a791ef8e729ee76e5b96e90fa3c499b96b
Niels Göttsch <ng@ife.de>
2025-04-05 15:50:56 +0200

    [WIP] account_payment_portal: first draft

---
+++ account_payment_portal/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Account Payment Portal",
+    "summary": "Add a Portal View for Customer Payments",
+    "author": "BAKEUP",
+    "website": "https://www.bakeup.org",
+    "category": "Accounting",
+    "version": "16.0.1.0.0",
+    "depends": ["account", "portal"],
+    "data": ["security/account_security.xml", "views/portal_templates.xml"],
+    "license": "LGPL-3",
+}
