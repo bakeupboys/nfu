c22fc9ad2d6ae0e9c0877674de9141512aa0cd22
Niels Göttsch <ng@ife.de>
2025-07-24 11:37:55 +0200

    [IMP] nfu_account: add credit menu

---
+++ nfu_account/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "NFU Account",
+    "summary": "Adds EKG related features to accounting",
+    "author": "BAKEUP",
+    "website": "https://www.bakeup.org",
+    "category": "Stock",
+    "version": "16.0.1.0.0",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "license": "LGPL-3",
+    "application": True,
+}
