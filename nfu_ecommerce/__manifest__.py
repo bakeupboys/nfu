54c7d206122b75eda63fb9fcd6e67e67f6336c3a
Niels Göttsch <ng@ife.de>
2025-07-29 13:38:29 +0200

    [IMP] nfu_ecommerce: add credit to portal details

--- nfu_ecommerce/__manifest__.py
+++ nfu_ecommerce/__manifest__.py
@@ -4,13 +4,14 @@
     "author": "BAKEUP",
     "website": "https://www.bakeup.org",
     "category": "website",
-    "version": "16.0.1.4.0",
+    "version": "16.0.1.4.1",
     "depends": [
         "sale_product_configurator",
         "website_sale",
         "nfu_sale_order_batch_packaging",
         "website_decimal_quantity",
         "nfu_product_bnn_attributes",
+        "nfu_account",
     ],
     "data": [
         "views/res_config_settings_views.xml",
@@ -20,6 +21,7 @@
     "assets": {
         "web.assets_frontend": [
             "nfu_ecommerce/static/src/js/*.js",
+            "nfu_ecommerce/static/src/scss/portal.scss",
         ]
     },
     "license": "LGPL-3",
