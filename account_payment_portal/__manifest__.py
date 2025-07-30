c509a1be9bcacb5c5a3f05bcbf0bc94360537e9f
madmooose <31863594+madmooose@users.noreply.github.com>
2025-07-30 12:26:09 +0200

    [112] [ADD] account_payment_portal
    * [WIP] account_payment_portal: first draft
    
    * [ADD] account_payment_portal
    
    * [IMP] account_payment_portal: Fix user rights and add test
    
    ---------
    
    Co-authored-by: Niels Göttsch <ng@ife.de>

---
+++ account_payment_portal/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Account Payment Portal",
+    "summary": "Add a Portal View for Customer Payments",
+    "author": "BAKEUP",
+    "website": "https://www.bakeup.org",
+    "category": "Accounting",
+    "version": "16.0.1.0.2",
+    "depends": ["account", "portal"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/account_security.xml",
+        "views/portal_templates.xml",
+    ],
+    "license": "LGPL-3",
+}
