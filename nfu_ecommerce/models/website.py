bf3eed8669034b20fbb18fe9a6d569662ebd0e90
Niels Göttsch <ng@ife.de>
2025-08-27 12:32:52 +0200

    [FIX] nfu_ecommerce: Drop session if SO belongs to another website (Odoo 16 bug)

--- nfu_ecommerce/models/website.py
+++ nfu_ecommerce/models/website.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from odoo.http import request
 
 
 class Website(models.Model):
@@ -10,3 +11,21 @@ class Website(models.Model):
         help="When the credit of a customer is lower than the warning level the credit is shown in red on the website.",
     )
     avoid_depts = fields.Boolean(string="Avoid depths")
+
+    def sale_get_order(self, force_create=False, update_pricelist=False):
+        """Override to clean sale_order_id if so belongs to another website"""
+        self.ensure_one()
+
+        self = self.with_company(self.company_id)
+        SaleOrder = self.env["sale.order"].sudo()
+
+        sale_order_id = request.session.get("sale_order_id")
+
+        if sale_order_id:
+            sale_order = SaleOrder.browse(sale_order_id).exists()
+            if sale_order and sale_order.website_id != self:
+                request.session.pop("sale_order_id")
+
+        return super().sale_get_order(
+            force_create=force_create, update_pricelist=update_pricelist
+        )
