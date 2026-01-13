93a01cc479fc19a15bd4ae8911c1c7d5853209a8
Niels Göttsch <ng@ife.de>
2026-01-13 20:32:42 +0100

    [FIX] data_nature_importer: move image encoding into utils

--- data_nature_importer/models/product_template.py
+++ data_nature_importer/models/product_template.py
@@ -1,5 +1,3 @@
-import base64
-
 from odoo import models
 
 from .. import utils as dn_utils
@@ -12,7 +10,7 @@ class ProductTemplate(models.Model):
         for product in self:
             image_data = product._get_dn_image()
             if image_data:
-                product.image_1920 = base64.b64encode(image_data).decode("ascii")
+                product.image_1920 = image_data
 
     def _get_dn_metadata(self):
         self.ensure_one()
@@ -29,5 +27,5 @@ class ProductTemplate(models.Model):
         image_id = metadata.get("images")[0].get("id")
         image_data = dn_utils.get_datanature_image(self, image_id, token=None)
         if image_data:
-            return base64.b64encode(image_data).decode("ascii")
+            return image_data
         return False
